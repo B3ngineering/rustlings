c0248071187a0d2052060dc16f471f0af11ff1f9
B3ngineering <98676125+B3ngineering@users.noreply.github.com>
2024-10-20 19:21:19 -0300

    Quiz 2 and options

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,23 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+        for (string, command) in input {
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(usize) => {
+                    let mut bar = string.clone();
+                    for _ in 0..usize {
+                        bar.push_str("bar");
+                    }
+                    output.push(bar);
+                }
+            }
+        }
+        output
+    }
 }
 
 fn main() {
@@ -39,6 +56,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
